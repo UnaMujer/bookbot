865bacd2cfae763c47e4594d5180935b34b79126
UnaMujer <lucia.chovan87@gmail.com>
2025-03-12 21:04:02 +0100

    update readme with a link

--- test.py
+++ test.py
@@ -5,10 +5,27 @@ def main():
     print(f"{num_words} words found in the document")
 
 
+def sort_on(dict):
+    return dict["num"]
+
+
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+chars.sort(reverse=True, key=sort_on)
+print(vehicles)
 
 def get_book_text(path):
     with open(path) as f:
